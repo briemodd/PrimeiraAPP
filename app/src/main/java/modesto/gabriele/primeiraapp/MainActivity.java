f4575d3c333f887edc382f5265299a1f405f4817
20221TIIMI0319 <bibiairam1@gmail.com>
2024-03-26 09:13:51 -0300

    Início dos comentários em MainActivity.

--- app/src/main/java/modesto/gabriele/primeiraapp/MainActivity.java
+++ app/src/main/java/modesto/gabriele/primeiraapp/MainActivity.java
@@ -9,15 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    //A classe main activity é a parte principal da aplicacao
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // onCreate é chamado toda vez que uma tela está sendo criada
+
         super.onCreate(savedInstanceState);
+        // setContentView cria os elementos de interface
         setContentView(R.layout.activity_main);
+        // Obtem o botao Enviar atraves do id
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        // setOnClickListener define o que acontece quando se clica no botao
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { // é executado quando o botao é clicado
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
                 String textoDigitado = etDigiteAqui.getText().toString();
                 Intent i = new Intent(MainActivity.this, NextActivity.class);
